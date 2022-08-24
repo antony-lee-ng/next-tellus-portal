98f219884c0c80d51e4c60fb51f612cdc61f2afb
Kingfapa <14345584+Kingfapa@users.noreply.github.com>
2022-08-24 20:13:08 +0200

    first draft of new export

---
+++ tools/export.ts
@@ -0,0 +1,10 @@
+import { mkdirp } from "fs-extra";
+import path from "path";
+
+(async () => {
+  const targetPath = path.join("//slldrifttools01.gaia.sll.se", "tellus");
+  await mkdirp(
+    path.join(targetPath, "builds", process.env.npm_package_version)
+  );
+  // await mkdir("//slldrifttools01.gaia.sll.se/tellus/hello");
+})();
