9475ea5fb831b9bdec7a3183db7af0147dbe5507
Kingfapa <14345584+Kingfapa@users.noreply.github.com>
2022-08-25 16:58:53 +0200

    cleanup and better typings

--- src/lib/TellusAPI.ts
+++ src/lib/TellusAPI.ts
@@ -1,21 +1,14 @@
-import axios, { AxiosError, AxiosInstance } from "axios";
-import { join } from "path";
+import axios, { AxiosInstance } from "axios";
 import { loadConfig } from "./config";
 import { logger } from "./logger";
 import { queueHandler } from "./QueueHandler";
 import { formSchema } from "./schema";
-import { outputFile } from "fs-extra";
-import { readFile } from "fs/promises";
 import https from "https";
-export interface IServices {
-  result: {
-    sys_id: string;
-    name: string;
-  }[];
+export interface IResult {
+  result: { number: string; success: boolean };
 }
 export class TellusAPI {
   private instance: AxiosInstance;
-  path: string;
   constructor() {
     const { ENDPOINT, USERNAME, PASSWORD } = loadConfig(process.env.NODE_ENV);
     this.instance = axios.create({
@@ -28,15 +21,12 @@ export class TellusAPI {
         rejectUnauthorized: false,
       }),
     });
-    this.path = process.env.DATA_PATH || "./data/";
   }
 
   async createCall(formData: Record<any, any>, addToQueueIfFail: boolean) {
     try {
       logger.info("creating call");
-      const { data } = await this.instance.post<{
-        result: { number: string; success: boolean };
-      }>(
+      const { data } = await this.instance.post<IResult>(
         `/api/global/external_portal/create`,
         await formSchema.validate(formData)
       );
@@ -53,36 +43,6 @@ export class TellusAPI {
         logger.error(errMsg);
         throw new Error(errMsg);
       }
-    }
-  }
-
-  async fetchServices(): Promise<IServices> {
-    const path = join(this.path, "services.json");
-    try {
-      logger.info("fetchServices init");
-      const { data } = await this.instance.get<IServices>(
-        "/api/now/table/cmdb_ci_service",
-        {
-          params: {
-            sysparm_query: "",
-            sysparm_fields: "name,sys_id",
-            sysparm_limit: 10,
-          },
-        }
-      );
-
-      // Save the data, if the endpoint goes down
-      await outputFile(path, JSON.stringify(data));
-
-      logger.info(`Saved services to -> ${join(this.path, "services.json")}`);
-
-      return data;
-    } catch (err) {
-      logger.error(`fetchServices failed: ${(err as Error).message}`);
-      // @ts-ignore
-      logger.error((err as AxiosError).config);
-
-      return JSON.parse(await readFile(path, "utf-8")) || { result: [] };
     }
   }
 }
