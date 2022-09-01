2646650926e195b7b7f8f9e3c842c82b2a2041fb
Kingfapa <14345584+Kingfapa@users.noreply.github.com>
2022-09-01 12:39:14 +0200

    better typescript support

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -65,7 +65,7 @@ export const FormLayout = () => {
         caller: "",
         user_name: "",
         u_place_of_work: "",
-        u_alternativ_kontaktvag: undefined,
+        u_alternativ_kontaktvag: "" as unknown as number, // This is because of resetting form does not work when using undefined
         u_additional_e_mail_address: "",
         call_type: "sc_request",
         short_description: "",
