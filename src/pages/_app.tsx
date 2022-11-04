761b01b7ecac04a19eff91a7a1ed8486372dc3c6
Kingfapa <14345584+Kingfapa@users.noreply.github.com>
2022-11-04 14:06:47 +0100

    redirect to indexpage if 404

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import App, { AppContext } from 'next/app'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +10,19 @@ function MyApp({ Component, pageProps }: AppProps) {
       {/* <DarkModeSwitch /> */}
     </ChakraProvider>
   );
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext)
+
+  if (appContext.ctx.res?.statusCode === 404) {
+    appContext.ctx.res.writeHead(302, { Location: '/' })
+    appContext.ctx.res.end()
+    return
+  }
+
+  return { ...appProps }
 }
 
 export default MyApp;
